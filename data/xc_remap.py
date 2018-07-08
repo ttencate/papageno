a403942d95a9219536e9ce75972bfb5cb9087069
Thomas ten Cate <ttencate@gmail.com>
2018-07-08 14:00:52 +0200

    Add scraper for XC metadata that is not present in the API

---
+++ data/xc_remap.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import logging
+import re
+import sys
+
+from ioc import ioc
+from xenocanto import xenocanto
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    ioc_list = ioc.IOCList()
+
+    queries = sys.stdin
+
+    first = True
+    for line in queries:
+        query = re.sub(r'#.*', '', line).strip()
+        if not query:
+            continue
+
+        recordings = xenocanto.find_recordings_cached(query)
+        if recordings:
+            print(query)
+        else:
+            species = ioc_list.find_by_name(query)
+            if species:
+                alt_names = set(name for name in species.alt_names if name != query)
+                found_names = set()
+                for alt_name in alt_names:
+                    if alt_name == query:
+                        continue
+                    recordings = xenocanto.find_recordings_cached(alt_name)
+                    if recordings:
+                        found_names.add(alt_name)
+                if not found_names:
+                    logging.error('No results for query "%s", nor for synonyms %s',
+                            query,
+                            ', '.join("%s" % name for name in alt_names))
+                else:
+                    logging.info('No results for query "%s", but results exist for %s',
+                            query,
+                            ', '.join("%s" % name for name in found_names))
+                    for name in sorted(found_names):
+                        print(name)
+            else:
+                logging.error('No results for query "%s", and no synonyms found in IOC list', query)
+                continue
+
+
+if __name__ == '__main__':
+    main()
+
--- data/xenocanto/xenocanto.py
+++ data/xenocanto/xenocanto.py
@@ -7,7 +7,14 @@ import os.path
 import re
 import urllib.parse
 
+from bs4 import BeautifulSoup
 import requests
+
+
+def get_cache_dir(subdir):
+    cache_dir_name = os.path.join(os.path.dirname(__file__), 'cache', subdir)
+    os.makedirs(cache_dir_name, exist_ok=True)
+    return cache_dir_name
 
 
 def make_url(query_dict):
@@ -16,6 +23,17 @@ def make_url(query_dict):
     PATH = '/api/2/recordings'
     query = urllib.parse.urlencode(query_dict)
     return urllib.parse.urlunparse((SCHEME, HOSTNAME, PATH, None, query, None))
+
+
+def get_response(url):
+    if url.startswith('//'):
+        url = 'https:' + url
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except Exception as ex:
+        raise RuntimeError('Error downloading %s' % url) from ex
+    return response
 
 
 def find_recordings(query):
@@ -28,20 +46,12 @@ def find_recordings(query):
     page = 1
     while True:
         url = make_url({'query': query, 'page': str(page)})
-        response = requests.get(url)
-        response.raise_for_status()
-        json = response.json()
+        json = get_response(url).json()
         for recording in json['recordings']:
             yield recording
         if json['page'] >= json['numPages']:
             break
         page += 1
-
-
-def get_cache_dir(subdir):
-    cache_dir_name = os.path.join(os.path.dirname(__file__), 'cache', subdir)
-    os.makedirs(cache_dir_name, exist_ok=True)
-    return cache_dir_name
 
 
 def find_recordings_cached(query):
@@ -61,19 +71,12 @@ def find_recordings_cached(query):
 
 
 def download_recording(recording):
-    url = recording['file']
-    if url.startswith('//'):
-        url = 'https:' + url
-    response = requests.get(url)
-    try:
-        response.raise_for_status()
-    except Exception as ex:
-        raise RuntimeError('Error downloading %s' % url) from ex
+    response = get_response(recording['file'])
     return response.headers['content-type'], response.content
 
 
 def download_recording_cached(recording):
-    cache_dir_name = get_cache_dir('recordings')
+    cache_dir_name = get_cache_dir('audio')
     cache_file_names = glob.glob(os.path.join(cache_dir_name, '%s.*' % recording['id']))
     if len(cache_file_names) > 1:
         raise RuntimeError('Amgiguous cache for XC%s: %s' % (recording['id'], ', '.join(cache_file_names)))
@@ -91,3 +94,93 @@ def download_recording_cached(recording):
         with open(cache_file_name, 'wb') as cache_file:
             cache_file.write(data)
         return cache_file_name
+
+
+def fetch_metadata(recording):
+    url = recording['url']
+    response = get_response(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    def get_table(title):
+        for sibling in soup.find('h2', string=re.compile(title)).next_siblings:
+            if hasattr(sibling, 'name') and sibling.name == 'table':
+                return sibling
+    def get_table_cell(table, key, regexp=None, convert=None):
+        result = table.find('td', string=re.compile(key)).next_sibling.get_text(strip=True)
+        if result == 'Not specified':
+            return None
+        if regexp:
+            match = regexp.search(result)
+            if not match:
+                return None
+            result = match.group(1)
+        if convert:
+            result = convert(result)
+        return result
+    def absolute_url(relative_url):
+        return urllib.parse.urljoin(url, relative_url)
+
+    recording_data = get_table('Recording data')
+    audio_file_properties = get_table('Audio file properties')
+    sound_characteristics = get_table('Sound characteristics')
+
+    int_re = re.compile(r'(\d+)')
+    float_re = re.compile(r'(\d+|\d*\.\d+|\d+\.\d*)')
+    time_re = re.compile(r'(\d\d:\d\d)')
+    date_re = re.compile(r'(\d\d\d\d-\d\d-\d\d)')
+
+    name = soup.find(itemprop='name')
+    species = soup.find(class_='sci-name').string.split()
+    quality = soup.find(id=re.compile(r'^rating-.*$'), class_='selected')
+    sonogram_link = soup.find('a', string=re.compile(r'Download full-length sonogram'))
+
+    return {
+            # Attributes from the API (https://www.xeno-canto.org/article/153)
+            'id': re.match(r'XC(\d+)', name.contents[0]).group(1),
+            'gen': species[0],
+            'sp': species[1],
+            'ssp': species[2] if len(species) >= 3 else None,
+            'en': name.find('a').string.strip(),
+            'rec': get_table_cell(recording_data, 'Recordist'),
+            'cnt': get_table_cell(recording_data, 'Country'),
+            'loc': get_table_cell(recording_data, 'Location'),
+            'lat': get_table_cell(recording_data, 'Latitude', convert=float),
+            'lng': get_table_cell(recording_data, 'Longitude', convert=float),
+            'type': get_table_cell(sound_characteristics, 'Type'),
+            'file': absolute_url(soup.find('a', string=re.compile(r'Download audio file'))['href']),
+            'lic': absolute_url(soup.find(string=re.compile('^\s*Creative Commons')).parent['href']),
+            'url': url,
+            'q': quality.string if quality else None,
+            'time': get_table_cell(recording_data, 'Time', regexp=time_re),
+            'date': get_table_cell(recording_data, 'Date', regexp=date_re),
+
+            # Attributes not present in the API.
+            'sonogram_url': absolute_url(sonogram_link['href']) if sonogram_link else None,
+            'elevation_m': get_table_cell(recording_data, 'Elevation', regexp=int_re, convert=int),
+            'length_s': get_table_cell(audio_file_properties, 'Length', regexp=float_re, convert=float),
+            'sampling_rate_hz': get_table_cell(audio_file_properties, 'Sampling rate', regexp=int_re, convert=int),
+            'bitrate_bps': get_table_cell(audio_file_properties, 'Bitrate of mp3', regexp=int_re, convert=int),
+            'channels': get_table_cell(audio_file_properties, 'Channels', regexp=int_re, convert=int),
+            'volume': get_table_cell(sound_characteristics, 'Volume'),
+            'speed': get_table_cell(sound_characteristics, 'Speed'),
+            'pitch': get_table_cell(sound_characteristics, 'Pitch'),
+            'sound_length': get_table_cell(sound_characteristics, 'Length'),
+            'number_of_notes': get_table_cell(sound_characteristics, 'Number of notes'),
+            'variable': get_table_cell(sound_characteristics, 'Variable'),
+    }
+
+
+def fetch_metadata_cached(recording):
+    cache_dir_name = get_cache_dir('metadata')
+    cache_file_name = os.path.join(cache_dir_name, '%s.json' % recording['id'])
+    try:
+        with open(cache_file_name, 'rb') as cache_file:
+            return json.load(cache_file)
+    except FileNotFoundError:
+        pass
+
+    metadata = fetch_metadata(recording)
+    with open(cache_file_name, 'wt') as cache_file:
+        json.dump(metadata, cache_file, indent=2)
+    logging.info('Wrote metadata for XC%s to %s', recording['id'], cache_file_name)
+    return metadata
