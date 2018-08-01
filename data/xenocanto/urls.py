f6b4d24279f5f5844abe578d71d0de77d41143c4
Thomas ten Cate <ttencate@gmail.com>
2018-08-01 14:26:46 +0200

    Group recordings by species

--- data/xenocanto/urls.py
+++ data/xenocanto/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+        path('species', views.species_list, name='species_list'),
+        path('species/<str:ioc_name>', views.species, name='species'),
 ]
