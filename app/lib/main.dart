421286885555fc28ef7090cc2ed4e86668d3bf6d
Thomas ten Cate <ttencate@gmail.com>
2020-03-28 20:20:50 +0100

    Start working on the UI
    Former-commit-id: a41738ad71d18b5dbaff4412d80a317c4c342eb8

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,111 +1,194 @@
+import 'dart:collection';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var questionFactory = QuestionFactory();
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Papageno',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Text("Hello world") // QuestionWidget(questionFactory.createQuestion()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+enum Language {
+  // scientific,
+  english,
+  // dutch,
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Species {
+  final int id;
+  final Map<Language, String> _names;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Species(this.id, this._names) {
+    assert(Language.values.every((language) => _names.containsKey(language)));
   }
+
+  String nameIn(Language language) => _names[language];
+}
+
+class SpeciesDb {
+  final _species = HashMap<int, Species>();
+
+  SpeciesDb() {
+    for (final species in [
+      Species(1, {Language.english: "great tit"}),
+      Species(2, {Language.english: "common chaffinch"}),
+      Species(3, {Language.english: "common blackbird"}),
+      Species(4, {Language.english: "common chiffchaff"}),
+      Species(5, {Language.english: "European robin"}),
+      Species(6, {Language.english: "willow warbler"}),
+      Species(7, {Language.english: "Eurasian blue tit"}),
+      Species(8, {Language.english: "Eurasian blackcap"}),
+      Species(9, {Language.english: "red crossbill"}),
+      Species(10, {Language.english: "song thrush"}),
+    ]) {
+      _addSpecies(species);
+    }
+  }
+
+  List<int> get ids => _species.keys.toList();
+
+  Species species(int id) => _species[id];
+
+  void _addSpecies(Species species) {
+    assert(!_species.containsKey(species.id));
+    _species[species.id] = species;
+  }
+}
+
+class Recording {
+  final Species species;
+  final double durationSeconds;
+
+  Recording(this.species, this.durationSeconds);
+}
+
+class Question {
+  final Recording recording;
+  final List<Species> choices;
+
+  Question(this.recording, this.choices) {
+    assert(choices.isNotEmpty);
+    assert(choices.contains(answer));
+  }
+
+  Species get answer => recording.species;
+}
+
+class QuestionFactory {
+  final _random = Random();
+  final _answerCount = 4;
+  final _speciesDb = SpeciesDb();
+
+  Question createQuestion() {
+    var ids = _speciesDb.ids;
+    assert(ids.length >= _answerCount);
+    List<Species> choices = [];
+    for (var i = 0; i < _answerCount; i++) {
+      var index = _random.nextInt(ids.length);
+      choices.add(_speciesDb.species(ids[index]));
+      ids[index] = ids.last;
+      ids.removeLast();
+    }
+    var answer = choices[_random.nextInt(choices.length)];
+    return Question(Recording(answer, 5.0), choices);
+  }
+}
+
+class QuestionWidget extends StatefulWidget {
+  final Question _question;
+
+  QuestionWidget(this._question);
+
+  @override
+  _QuestionWidgetState createState() => _QuestionWidgetState(this._question);
+}
+
+class _QuestionWidgetState extends State<QuestionWidget> {
+  final Question _question;
+
+  _QuestionWidgetState(this._question);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have clicked the button this many times:',
+      // TODO horizontal layout
+      body: Column(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 16.0/9.0,
+            child: Placeholder(),
+          ),
+          Ink(
+            color: Colors.blue.shade50, // TODO take from theme
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Slider(
+                    value: 0.0,
+                    min: 0.0,
+                    max: _question.recording.durationSeconds,
+                    onChanged: (double value) {
+                      // TODO
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  color: Colors.blue, // TODO take from theme
+                  iconSize: 48.0,
+                  onPressed: () {
+                    // TODO
+                  },
+                ),
+              ]
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+          ListView(
+            padding: EdgeInsets.all(0),
+            shrinkWrap: true,
+            children: ListTile.divideTiles(
+              context: context,
+              tiles: _question.choices.map((species) {
+                return ListTile(
+                  title: Text(species.nameIn(Language.english).capitalize()),
+                  onTap: () {
+                    // TODO
+                  },
+                );
+              }),
+            ).toList(),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  List<Widget> _intersperseLines(Iterable<Widget> children) {
+    List<Widget> result = [];
+    result.add(Divider());
+    for (final child in children) {
+      result.add(child);
+      result.add(Divider());
+    }
+    return result;
+  }
+}
+
+extension Capitalize on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
   }
 }
