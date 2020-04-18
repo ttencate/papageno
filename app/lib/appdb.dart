ccc5d80bac26b4bb39d244f7d61004703a5ab1bc
Thomas ten Cate <ttencate@gmail.com>
2020-04-18 17:05:51 +0200

    Load database in app
    Former-commit-id: 8ab7b1f1e536f75fb56fb5e77f8411c2b4232bbc

---
+++ app/lib/appdb.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'model.dart';
+
+class NotFoundException implements Exception {
+  final dynamic what;
+  NotFoundException(this.what);
+}
+
+class AppDb {
+
+  final Database _db;
+
+  AppDb._fromDatabase(this._db);
+
+  static Future<AppDb> open() async {
+    // https://github.com/tekartik/sqflite/blob/master/sqflite/doc/opening_asset_db.md
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'app.db');
+    await deleteDatabase(path);
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (_) {}
+    // TODO stream rather than loading it all into RAM at once
+    final data = await rootBundle.load(join('assets', 'app.db'));
+    final bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    await File(path).writeAsBytes(bytes, flush: true);
+    final db = await openDatabase(path, readOnly: true);
+    return AppDb._fromDatabase(db);
+  }
+
+  Future<List<int>> allSpeciesIds() async {
+    final records = await _db.rawQuery('select species_id from species');
+    return records.map((r) => r['species_id'] as int).toList();
+  }
+
+  Future<Species> species(int speciesId) async {
+    final records = await _db.rawQuery('select * from species where species_id = ?', [speciesId]);
+    if (records.isEmpty) {
+      throw NotFoundException(speciesId);
+    }
+    final r = records[0];
+    return Species(
+        r['species_id'],
+        r['scientific_name'],
+        BuiltMap<LanguageCode, String>.build((builder) {
+          builder.addEntries(r.entries
+              .where((e) => e.key.startsWith('common_name_'))
+              .map((e) => MapEntry(languageCodeFromString(e.key.substring(12)), e.value)));
+        }));
+  }
+}
--- app/lib/model.dart
+++ app/lib/model.dart
@@ -1,51 +1,74 @@
 import 'dart:math';
 
-enum Language {
-  // scientific,
-  english,
-  dutch,
+import 'package:built_collection/built_collection.dart';
+import 'package:meta/meta.dart';
+
+import 'appdb.dart';
+
+/// Language codes for all supported languages.
+///
+/// This is entirely separate from the rest of the app's localization.
+/// Identifiers are prefixed with `language_` because `is` is a keyword.
+enum LanguageCode {
+  language_en,
+  language_af,
+  language_ca,
+  language_zh_CN,
+  language_zh_TW,
+  language_ics,
+  language_da,
+  language_nl,
+  language_et,
+  language_fi,
+  language_fr,
+  language_de,
+  language_hu,
+  language_is,
+  language_id,
+  language_it,
+  language_ja,
+  language_lv,
+  language_lt,
+  language_se,
+  language_no,
+  language_pl,
+  language_pt,
+  language_ru,
+  language_sk,
+  language_sl,
+  language_es,
+  language_sv,
+  language_th,
+  language_uk,
 }
 
+LanguageCode languageCodeFromString(String s) {
+  s = 'LanguageCode.language_' + s;
+  return LanguageCode.values.firstWhere(
+      (languageCode) => languageCode.toString() == s,
+      orElse: () { throw Exception('No language code ${s}; have: ${LanguageCode.values}'); },
+  );
+}
+
+@immutable
 class Species {
-  final int id;
-  final Map<Language, String> _names;
+  final int speciesId;
+  final String scientificName;
+  final BuiltMap<LanguageCode, String> commonNames;
 
-  Species(this.id, this._names) :
-        assert(Language.values.every((language) => _names.containsKey(language)));
+  Species(this.speciesId, this.scientificName, this.commonNames) :
+        assert(LanguageCode.values.every((language) => commonNames.containsKey(language)));
 
-  String nameIn(Language language) => _names[language];
-}
-
-class SpeciesDb {
-  final _species = <int, Species>{};
-
-  SpeciesDb() {
-    for (final species in [
-      Species(1, {Language.english: 'great tit', Language.dutch: 'koolmees'}),
-      Species(2, {Language.english: 'common chaffinch', Language.dutch: 'vink'}),
-      Species(3, {Language.english: 'common blackbird', Language.dutch: 'merel'}),
-      Species(4, {Language.english: 'common chiffchaff', Language.dutch: 'tjiftjaf'}),
-      Species(5, {Language.english: 'European robin', Language.dutch: 'roodborst'}),
-      Species(6, {Language.english: 'willow warbler', Language.dutch: 'fitis'}),
-      Species(7, {Language.english: 'Eurasian blue tit', Language.dutch: 'pimpelmees'}),
-      Species(8, {Language.english: 'Eurasian blackcap', Language.dutch: 'zwartkop'}),
-      Species(9, {Language.english: 'red crossbill', Language.dutch: 'witbandkruisbek'}),
-      Species(10, {Language.english: 'song thrush', Language.dutch: 'zanglijster'}),
-    ]) {
-      _addSpecies(species);
+  String commonNameIn(LanguageCode language) {
+    final commonName = commonNames[language];
+    if (commonName == null || commonName.isEmpty) {
+      return scientificName;
     }
-  }
-
-  List<int> get ids => _species.keys.toList();
-
-  Species species(int id) => _species[id];
-
-  void _addSpecies(Species species) {
-    assert(!_species.containsKey(species.id));
-    _species[species.id] = species;
+    return commonName;
   }
 }
 
+@immutable
 class Recording {
   final Species species;
   final String fileName;
@@ -67,22 +90,29 @@ class Question {
   bool isCorrect(Species species) => species == answer;
 }
 
-class QuestionFactory {
+class Quiz {
+
+  final int totalQuestionCount = 20;
+  int currentQuestionIndex = 0;
+
   final _random = Random();
   final _answerCount = 4;
-  final _speciesDb = SpeciesDb();
+  final AppDb _appDb;
 
-  Question createQuestion() {
-    var ids = _speciesDb.ids;
+  Quiz(this._appDb);
+
+  Future<Question> getNextQuestion() async {
+    var ids = await _appDb.allSpeciesIds();
     assert(ids.length >= _answerCount);
     var choices = <Species>[];
     for (var i = 0; i < _answerCount; i++) {
       var index = _random.nextInt(ids.length);
-      choices.add(_speciesDb.species(ids[index]));
+      choices.add(await _appDb.species(ids[index]));
       ids[index] = ids.last;
       ids.removeLast();
     }
     var answer = choices[_random.nextInt(choices.length)];
+    currentQuestionIndex++;
     return Question(Recording(answer, 'recordings/common_blackbird.mp3'), choices);
   }
 }
