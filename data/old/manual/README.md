9388532f2e40051f1e173e159dc859da04089361
Thomas ten Cate <ttencate@gmail.com>
2020-04-24 15:36:35 +0200

    Remove old cruft
    Former-commit-id: 7b9e015f404b76de6528d8b0806292c5ca599cc4
    Former-commit-id: 31952a7c43444b24aa76e4f93969bfe10eab231b

--- data/old/manual/README.md
+++
@@ -1,1 +0,0 @@
-These files were typed in by hand. Attribution unlikely to be required.
--- data/old/xenocanto/admin.py
+++
@@ -1,9 +0,0 @@
-'''
-Admin site configuration for the xenocanto app.
-'''
-
-from django.contrib import admin
-
-from .models import Recording
-
-admin.site.register(Recording)
