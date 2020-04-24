6f056c08d2b9e849e23fa96985b7b6ad74770671
Thomas ten Cate <ttencate@gmail.com>
2020-04-24 18:05:18 +0200

    Update regions_to_gpkg and QGIS project
    Former-commit-id: 06ddcb11755ae35a024f0037242f631f177fb636
    Former-commit-id: f2965d4492730a4a14d6378ba68d4d356f3a09dc

--- data/regions_to_gpkg.py
+++ data/regions_to_gpkg.py
@@ -10,7 +10,7 @@ from osgeo import gdal, ogr, osr
 from sqlalchemy.orm import joinedload
 
 from regions import Region
-from species import Species
+from species import Species, SelectedSpecies
 
 
 def add_args(parser):
@@ -32,6 +32,13 @@ def main(args, session):
             .options(joinedload(Species.common_names))
     }
 
+    logging.info(f'Loading selected species')
+    selected_scientific_names = set(
+        species.scientific_name
+        for species in session.query(Species)\
+            .join(SelectedSpecies)
+    )
+
     try:
         os.remove(args.gpkg_file)
         logging.info(f'Deleted previous file {args.gpkg_file}')
@@ -43,22 +50,35 @@ def main(args, session):
     data_source = ogr.GetDriverByName('GPKG').CreateDataSource(args.gpkg_file)
     layer = data_source.CreateLayer('regions', geom_type=ogr.wkbPolygon, srs=wgs84)
     layer.CreateField(ogr.FieldDefn('region_id', ogr.OFTInteger))
-    layer.CreateField(ogr.FieldDefn('num_species', ogr.OFTInteger))
-    layer.CreateField(ogr.FieldDefn('num_recordings', ogr.OFTInteger))
-    layer.CreateField(ogr.FieldDefn('ranked_species_en', ogr.OFTString))
-    layer.CreateField(ogr.FieldDefn('ranked_species_nl', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('num_observed_species', ogr.OFTInteger))
+    layer.CreateField(ogr.FieldDefn('num_selected_species', ogr.OFTInteger))
+    layer.CreateField(ogr.FieldDefn('total_weight', ogr.OFTInteger))
+    layer.CreateField(ogr.FieldDefn('ranked_observed_species_en', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('ranked_observed_species_nl', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('ranked_selected_species_en', ogr.OFTString))
+    layer.CreateField(ogr.FieldDefn('ranked_selected_species_nl', ogr.OFTString))
 
     for region in session.query(Region):
-        if region.num_recordings() > 0:
+        if region.num_species() > 0:
             feature = ogr.Feature(layer.GetLayerDefn())
             feature.SetGeometry(ogr.CreateGeometryFromWkt(region.to_wkt()))
             feature['region_id'] = region.region_id
-            feature['num_species'] = region.num_species()
-            feature['num_recordings'] = region.num_recordings()
-            feature['ranked_species_en'] = ', '.join(
+            feature['num_observed_species'] = region.num_species()
+            feature['num_selected_species'] = len(
+                set(region.scientific_names).intersection(selected_scientific_names))
+            feature['total_weight'] = region.total_weight()
+            feature['ranked_observed_species_en'] = ', '.join(
                 common_names.get(scientific_name, {}).get('en') or '?'
                 for scientific_name in region.ranked_scientific_names())
-            feature['ranked_species_nl'] = ', '.join(
+            feature['ranked_observed_species_nl'] = ', '.join(
                 common_names.get(scientific_name, {}).get('nl') or '?'
                 for scientific_name in region.ranked_scientific_names())
+            feature['ranked_selected_species_en'] = ', '.join(
+                common_names.get(scientific_name, {}).get('en') or '?'
+                for scientific_name in region.ranked_scientific_names()
+                if scientific_name in selected_scientific_names)
+            feature['ranked_selected_species_nl'] = ', '.join(
+                common_names.get(scientific_name, {}).get('nl') or '?'
+                for scientific_name in region.ranked_scientific_names()
+                if scientific_name in selected_scientific_names)
             layer.CreateFeature(feature)
