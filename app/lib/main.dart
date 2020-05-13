f3f96949196cd4982c19017de6fbf29fe1697a7a
Thomas ten Cate <ttencate@gmail.com>
2020-05-13 14:03:34 +0200

    Lock screen to portrait mode
    Fixes #17

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'ui/app.dart';
 
 void main() {
-  runApp(App());
+  // Forcing portrait mode until we implement landscape layouts (https://github.com/ttencate/papageno/issues/16)
+  // https://stackoverflow.com/a/52720581/14637
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+    .then((_) {
+      runApp(App());
+    });
 }
